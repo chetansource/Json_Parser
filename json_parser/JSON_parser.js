49ee9203227c106dd7663804dd96095bd01e16ef
chetansource <chetankumargopi149@gmail.com>
2022-10-17 15:51:42 +0530

    fixed all errors

--- json_parser/JSON_parser.js
+++ json_parser/JSON_parser.js
@@ -1,5 +1,4 @@
 //code for each json types
-
 let null_parser= input =>{
     input=input.trim()
     if(input.startsWith('null')){ 
@@ -9,7 +8,7 @@ let null_parser= input =>{
 }
 
 let boolean_parser = input =>{
-    input=input.trim()
+     input=input.trim()
      if(input.startsWith('true')) return[true,input.slice(4)]
      if(input.startsWith('false')) return[false,input.slice(5)]
      return null
@@ -17,114 +16,90 @@ let boolean_parser = input =>{
 
 let number_parser = function(input){
     input=input.trim()
-    let regex=/^-?([1-9](\d+)?|0)(\.\d+?)?([Ee][\-\+]?\d+)?/
-        regoutput = input.match(regex)
-        // console.log(regoutput)
-        if( regoutput !== null){
-                return[regoutput[0],input.slice(regoutput[0].length)]
-        }
-        return null
-
+    let regex=/^-?([1-9]\d*|0)(\.\d+)?([Ee][-+]?\d+)?/
+    regoutput = input.match(regex)
+    if(regoutput === null) return null
+    return[Number(regoutput[0]),input.slice(regoutput[0].length)]
 }
 
-function string_parser(input){
+function string_parser (input) {
     input=input.trim()
-    if(!input.startsWith('"'))return null
-    let outputstring=''
-    const esccharacters=['"','\\','/','b','f','n','r','t','u']
+    if(!input.startsWith('"')) return null
     input=input.slice(1)
-    while(input.length){
-        if(input[0] === '"'){
-            return [outputstring,input.slice(1)]
-        }
-          else {
-                if(input[0].match(/[\u0000-\u001f]/i)) return null
-                if(input[0] === '\\'){
-                    if(input[1].startsWith(esccharacters)){
-                        outputstring +=input[1]
-                        input=input.slice(2)
-                    }
-                    else if( input[1] === 'u'){
-                        if(input.slice(2,6).match(/[\da-f]{4}/) === null) return null
-                        outputstring += String.fromCharCode(parseInt(input.slice(2,6),16))
-                        input=input.slice(6)
-                    }
-                    else {null}
-                }
-                else{
-                    outputstring += input[0]
-                    input=input.slice(1)
-                }
+    parsedstring =''
+    const esccharacters=['"','\\','/','b','f','n','r','t','u']
+    const invalid=[9,10] //tab and linebreak
+    let i=0
+    while(!input[i].startsWith('"')){
+        if(invalid.includes(input[i].charCodeAt(0)))return null
+        if(input[i] === '\\'){
+           if(!esccharacters.includes(input[i+1])) return null
+            if(input[i+1] === 'u'){
+                i +=4
             }
-
+            i +=2
         }
-     return null
+        else i++
+    }
+    return [input.slice(0,i),input.slice(i+1)]
+    }
+
+function value_parser(input){
+    input=input.trim()
+    return null_parser(input) || boolean_parser(input) || number_parser(input) || string_parser(input) || array_parser(input) || object_parser(input)
 }
 
 function array_parser(input){
     input=input.trim()
-    let array=[]
     if(!input.startsWith('['))return null
-    let value =input.slice(1)
-    while(!value.startsWith(']')){
-        value = value.trim()
-        value = value_parser(value)
-        if(value === null) return null
-        else{
-            array.push(value[0])
-            value=value[1].trim()
-            // console.log(value)
-            if(value.startsWith(',')){
-                value=value.slice(1).trim()
-                // value=value.slice(0).trim()
-            }
+    input =input.slice(1).trim() 
+    let array=[]
+    while(input[0] !==']'){
+        input = value_parser(input)   
+        if(input === null) return null
+        array.push(input[0])
+        input=input[1].trim()
+        if(input[0] === ','){
+            input=input.slice(1).trim()
+            if(input[0] === ']') return null
         }    
-        
     }
-    return [array,value.slice(1)]
+    return [array,input.slice(1)]
 }
-// console.log(array_parser('[  1, 12, ]'))
 
 function object_parser(input){
     input=input.trim()
-    let value=input.slice(1)
-    let object={}
     if(!input.startsWith('{')) return null
-    while(!value.startsWith('}')){
-        value=value.trim()
-        value=string_parser(value)
-        if(value === null) return null
-        let key = value[0]
-        value=value[1].trim()
-        if(!value.startsWith(':') ) return null
-        value = value_parser(value.slice(1))
-        if(value === null) return null
-        object[key] = value[0]
-        value=value[1].trim()
-        if(value.startsWith(',')){
-            value=value.slice(1).trim()
-        } 
-    
+    input=input.slice(1).trim()
+    let object={}
+    while(input[0] !== '}'){
+        input=string_parser(input)
+        if(input === null) return null
+        let key = input[0]
+        input=input[1].trim()
+        if(!input.startsWith(':')) return null
+        input=input.slice(1)
+        input = value_parser(input)
+        if(input === null) return null
+        object[key] = input[0]
+        input=input[1].trim()
+        if(input[0] === ','){ 
+            input=input.slice(1).trim()
+            if(input[0] === '}') return null
+        }
     }
-    return [object,value.slice(1)]  
-
-}
-// console.log(object_parser('{"obj1":1,"obj2":2,"12":12, }'))
-
-function value_parser(input){
-    return null_parser(input) || boolean_parser(input) || number_parser(input) || string_parser(input) || array_parser(input) || object_parser(input)
+    return [object,input.slice(1)]
 }
 
 function json_Parser(input){
     let parsevalue = array_parser(input) || object_parser(input)
-    if(parsevalue === null )return null
-    // console.log(parsevalue[1].length)
+    if(parsevalue === null || parsevalue[1].length > 0)return null 
     return parsevalue[0]
 }
 
 const fs = require('fs')//fail test cases
 for( let i=1; i <= 33 ;i++ ){
-    if(i != 18){
+    if(i !== 18 ){
         const data = fs.readFileSync(`./test/fail${i}.json`, 'utf8')
         console.log(`fail${i}`, json_Parser(data))
     }
